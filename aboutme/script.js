f0a989fca66b3456993d5e5c8827dc556912e5ed
watchmypizza <kevinosswald@proton.me>
2024-10-27 18:06:06 +0100

    Added github connections on about me page, aswell as other projects button

--- aboutme/script.js
+++ aboutme/script.js
@@ -1,4 +1,4 @@
-addEventListener('DOMContentLoaded', function() {
+addEventListener('DOMContentLoaded', function () {
     const backbtn = document.querySelector('.back');
     const discord = document.getElementById('discord');
     const tiktok = document.getElementById('tiktok');
@@ -6,23 +6,30 @@ addEventListener('DOMContentLoaded', function() {
     const spotify = document.getElementById('spotify');
     const steam = document.getElementById('steam');
 
-    backbtn.addEventListener('click', function() {
+    backbtn.addEventListener('click', function () {
         window.location.href = "../index.html";
     });
 
-    discord.addEventListener('click', function() {
+    discord.addEventListener('click', function () {
         window.open('https://discord.gg/jNKSt4xBbW');
     });
-    tiktok.addEventListener('click', function() {
+    tiktok.addEventListener('click', function () {
         window.open('https://www.tiktok.com/@islippedonsoappbf');
     });
-    roblox.addEventListener('click', function() {
+    roblox.addEventListener('click', function () {
         window.open('https://www.roblox.com/users/574981033/profile');
     });
-    spotify.addEventListener('click', function() {
+    spotify.addEventListener('click', function () {
         window.open('https://open.spotify.com/user/31ml73pq3gkmdw7w5mucy6ayw2tm?si=d6e25c36404c4ab7');
     });
-    steam.addEventListener('click', function() {
+    steam.addEventListener('click', function () {
         window.open('https://steamcommunity.com/profiles/76561198840944668');
+    });
+    github.addEventListener('click', function () {
+        window.open('https://github.com/watchmypizza');
+    });
+
+    otherproj.addEventListener('click', function () {
+        window.open('https://github.com/watchmypizza?tab=repositories');
     });
 });
